7de3852ef069874b6aa28ed3a51a3ee6a96af1b8
scilo7 <laurent.vasseur67@orange.fr>
2018-06-19 00:15:55 +0200

    inline admin

--- mjcg/adm/admin.py
+++ mjcg/adm/admin.py
@@ -2,11 +2,24 @@ from django.contrib import admin
 from .models import Membre, Activite, Tarif, Cotisation
 
 
+admin.site.register(Activite)
+# admin.site.register(Cotisation)
+
+
+class CotisationInline(admin.TabularInline):
+    model = Cotisation
+    extra = 0
+
+
+@admin.register(Tarif)
+class TarifAdmin(admin.ModelAdmin):
+    
+    list_filter = ('annee',)
+
+
 @admin.register(Membre)
 class MembreAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Activite)
-admin.site.register(Tarif)
-
+    
+    inlines = [
+        CotisationInline,
+    ]
